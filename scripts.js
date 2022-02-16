2f77c50e14b50c296ecf3ae75630e4a44ab3d6d0
Pedro H <pedro.bonel@gmail.com>
2022-02-16 20:34:41 -0300

    Converting binary to decimal

--- scripts.js
+++ scripts.js
@@ -2,15 +2,20 @@ function convertToDecimal (e) {
   if (!e) return null
   let result = 0
   const binary = e.target.value
-  const arr = Array.prototype.map
 
-  arr.call(binary,(char, idx) => result += Math.pow((Number(char) * 2), idx))
-  // arr.call(binary, (char, idx) => console.log(binary[idx]))
+  result = binary.split('').reverse().reduce((prev, val, idx) => {
+    return prev + (val * Math.pow(2, idx))
+  }, 0)
 
   const res = document.getElementById('result')
   return res.value = result
 }
 
+function maskToBinary (e) {
+  return e.key.replace(/\D/g, '')
+}
+
 const input = document.querySelector('.input-binary')
 
-input.addEventListener('keydown', convertToDecimal)
+input.addEventListener('keyup', maskToBinary)
+input.addEventListener('change', convertToDecimal)
